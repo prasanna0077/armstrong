b325e2ec9e977d4dbd7a729a1b058a22b1c7befb
Prasanna Rajasekaran <prasanna0077@hotmail.com>
2019-04-20 12:38:33 +0530

    Commit time changes

--- hello.java
+++ hello.java
@@ -1,7 +1,14 @@
+import java.util.*;
 class hello
 {
 	public static void main(String[] args)
 	{
+		// Instantiate a Date object
+	       	Date date = new Date();
+
+	       	// display time and date using toString()
+	       	System.out.println(date.toString());
+		
 		System.out.println("***********************");
 		System.out.println("Hello Dev Ops Learners");
 		System.out.println("Hello Simplilearn Users");
