6612bc73e717c81cb99f3cc2609dec5398c98818
Prasanna Rajasekaran <prasanna0077@hotmail.com>
2019-04-20 10:05:41 +0530

    Modified Comments - 2

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class hello
 {
 	public static void main(String[] args)
 	{
+		System.out.println("***********************");
 		System.out.println("Hello Dev Ops Learners");
 		System.out.println("Hello Simplilearn Users");
 		System.out.println("***********************");
