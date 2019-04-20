924b48e09c01e676a4fe247ca9fd0e87a9b2472e
Prasanna Rajasekaran <prasanna0077@hotmail.com>
2019-04-20 13:04:59 +0530

    Added Armstrong Program

--- hello.java
+++ hello.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Scanner;
 class hello
 {
 	public static void main(String[] args)
@@ -13,5 +14,23 @@ class hello
 		System.out.println("Hello Dev Ops Learners");
 		System.out.println("Hello Simplilearn Users");
 		System.out.println("***********************");
+
+		int num, number, temp, total = 0;
+		System.out.println("Ënter 3 Digit Number");
+		Scanner scanner = new Scanner(System.in);
+		num = scanner.nextInt();
+		scanner.close();
+		number = num;
+
+		for( ;number!=0;number /= 10)
+		{
+		    temp = number % 10;
+		    total = total + temp*temp*temp;
+		}
+
+		if(total == num)
+		    System.out.println(num + " is an Armstrong number");
+		else
+		    System.out.println(num + " is not an Armstrong number");
 	}
 }
